7cf277c605a3828ef82ece6fc8d21a16734647ad
Spencer Smith <smith,spence13@gmail.com>
2022-04-16 21:55:57 -0700

    added style sheet for later

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,30 @@
+import { useState } from "react";
+import { useQuery } from "react-query";
+//Material UI Components
+import { Drawer, Grid, LinearProgress, Badge } from "@material-ui/core";
+import AddShoppingCartIcon from "@material-ui/icons/AddShoppingCart";
+//Styles
+import { Wrapper } from "./App.styles";
+//Types
+export type CartItemType = {
+  id: number;
+  category: string;
+  description: string;
+  image: string; 
+  price: number;
+  title: string;
+  amount: number;
+}
 
+const getProducts = async (): Promise<CartItemType[]> =>
+  await(await fetch("https://fakestoreapi.com/products")).json();
 
 const App = () => {
+  const { data, isLoading, error } = useQuery<CartItemType[]>(
+    "products", 
+    getProducts
+    );
+    console.log(data);
   return (
     <div className="App">
       Start
